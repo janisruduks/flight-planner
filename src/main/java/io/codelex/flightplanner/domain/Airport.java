5d26136368be95a798cf06cbc69ed3e3a4e2d6ba
Jjanisruduks <janis.ruduks157@gmail.com>
2023-07-08 21:21:07 +0300

    Implemented main domain classes, flight and airport

--- src/main/java/io/codelex/flightplanner/domain/Airport.java
+++ src/main/java/io/codelex/flightplanner/domain/Airport.java
@@ -1,2 +1,60 @@
-package io.codelex.flightplanner.domain;public class Airport {
+package io.codelex.flightplanner.domain;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Objects;
+
+public class Airport {
+
+    @NotBlank(message = "Country is mandatory")
+    private String country;
+    @NotBlank(message = "City is mandatory")
+    private String city;
+    @NotBlank(message = "Airport location identifier is mandatory")
+    private String airport;
+
+    public Airport(String country, String city, String airport) {
+        this.country = country;
+        this.city = city;
+        this.airport = airport;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(@NotBlank String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(@NotBlank String city) {
+        this.city = city;
+    }
+
+    public String getAirport() {
+        return airport;
+    }
+
+    public void setAirport(@NotBlank String airport) {
+        this.airport = airport;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Airport airport1 = (Airport) o;
+        return country.equalsIgnoreCase(airport1.country)
+                && city.equalsIgnoreCase(airport1.city)
+                && airport.strip().equalsIgnoreCase(airport1.airport.strip());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, city, airport);
+    }
 }
--- src/main/java/io/codelex/flightplanner/domain/Flight.java
+++ src/main/java/io/codelex/flightplanner/domain/Flight.java
@@ -1,23 +1,88 @@
 package io.codelex.flightplanner.domain;
 
-public class Flights {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-    private String flightCode;
-    private String airlineName;
+import java.util.Objects;
 
-    public String getFlightCode() {
-        return flightCode;
+public class Flight {
+
+    @NotNull(message = "Airport from cannot be null")
+    @Valid
+    private Airport from;
+    @NotNull(message = "Airport to cannot be null")
+    @Valid
+    private Airport to;
+    @NotBlank(message = "Airline name is mandatory")
+    private String carrier;
+    @NotBlank(message = "Departure time is mandatory")
+    private String departureTime;
+    @NotBlank(message = "Arrival time is mandatory")
+    private String arrivalTime;
+
+    public Flight(Airport from, Airport to, String carrier, String departureTime, String arrivalTime) {
+        this.from = from;
+        this.to = to;
+        this.carrier = carrier;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
     }
 
-    public void setFlightCode(String flightCode) {
-        this.flightCode = flightCode;
+    public Airport getFrom() {
+        return from;
     }
 
-    public String getAirlineName() {
-        return airlineName;
+    public void setFrom(Airport from) {
+        this.from = from;
     }
 
-    public void setAirlineName(String airlineName) {
-        this.airlineName = airlineName;
+    public Airport getTo() {
+        return to;
+    }
+
+    public void setTo(Airport to) {
+        this.to = to;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flight flight = (Flight) o;
+        return Objects.equals(from, flight.from)
+                && Objects.equals(to, flight.to)
+                && Objects.equals(carrier, flight.carrier)
+                && Objects.equals(departureTime, flight.departureTime)
+                && Objects.equals(arrivalTime, flight.arrivalTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(to, carrier, departureTime, arrivalTime);
     }
 }
