39057fe101376d72e982094493c5db8974adc184
Jjanisruduks <janis.ruduks157@gmail.com>
2023-07-08 21:01:42 +0300

    Created simple security configuration with user manager, currently using deprecated password encoder for testing purposes
    Created data source config for future feature

--- src/main/java/io/codelex/flightplanner/config/SecurityConfig.java
+++ src/main/java/io/codelex/flightplanner/config/SecurityConfig.java
@@ -2,24 +2,38 @@ package io.codelex.flightplanner.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.List;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/testing-api/**").permitAll()
+                        .requestMatchers("/admin-api/**").authenticated()
+                        .anyRequest().permitAll()
                 )
-                        .httpBasic(Customizer.withDefaults());
+                .httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("codelex-admin")
+                .password("Password123")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
     }
 }
