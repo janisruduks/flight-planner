e992ce9edc0b33e8d15087c16d1e1c7f5b0ed916
Jjanisruduks <janis.ruduks157@gmail.com>
2023-07-08 21:01:42 +0300

    Created simple security configuration with user manager, currently using deprecated password encoder for testing purposes
    Created data source config for future feature

--- src/main/java/io/codelex/flightplanner/config/DataSourceConfiguration.java
+++ src/main/java/io/codelex/flightplanner/config/DataSourceConfiguration.java
@@ -1,9 +1,17 @@
 package io.codelex.flightplanner.config;
 
+import io.codelex.flightplanner.repository.FlightRepositoryInMemoryImpl;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-@Configuration
-public class DataSourceConfig {
 
-    //TODO implement data source switcher
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "flight-planner", name = "storage-type", havingValue = "in-memory")
+    public FlightRepositoryInMemoryImpl getInMemoryRepository() {
+        return new FlightRepositoryInMemoryImpl();
+    }
 
 }
