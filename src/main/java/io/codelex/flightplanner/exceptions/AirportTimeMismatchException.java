0c7b54555e5e011665057f50a77acfa1147f9e51
Jjanisruduks <janis.ruduks157@gmail.com>
2023-07-11 13:22:49 +0300

    Modified code, method delete flight by id now uses get flight by id
    Removed method that generated uuid and simply used UUID method call instead
    
    Refactored add flight method and airport date and time validation method, now add flight calls validate airports and dates method instead of 2 methods like before

--- src/main/java/io/codelex/flightplanner/exceptions/AirportTimeMismatchException.java
+++
@@ -1,7 +0,0 @@
-package io.codelex.flightplanner.exceptions;
-
-public class AirportTimeMismatchException extends RuntimeException {
-    public AirportTimeMismatchException() {
-        super("Airport arrival or departure time mismatch");
-    }
-}
