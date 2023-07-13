95b99f49d7ac314f4ea5d07856e7738ca0112f7a
Jjanisruduks <janis.ruduks157@gmail.com>
2023-07-13 13:26:52 +0300

    Added integration for creating flights and clearing all flights
    Added unit test for airport search

--- src/test/java/io/codelex/flightplanner/controllers/TestingApiControllerTest.java
+++ src/test/java/io/codelex/flightplanner/controllers/TestingApiControllerTest.java
@@ -1,4 +1,51 @@
+package io.codelex.flightplanner.controllers;
+
+import io.codelex.flightplanner.domain.Airport;
+import io.codelex.flightplanner.domain.Flight;
+import io.codelex.flightplanner.repository.FlightRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class TestingApiControllerTest {
-  
+
+    @Autowired
+    TestingApiController testingController;
+    @Autowired
+    FlightRepository flightRepository;
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("Should clean repository fully")
+    void clear() throws Exception {
+        Airport airportFrom = new Airport("LV", "Riga", "Rix");
+        Airport airportTo = new Airport("IT", "Milan", "BGY");
+        String departureTime = "2023-07-13 06:07";
+        String arrivalTime = "2023-07-16 12:04";
+        Flight flight = new Flight(airportFrom, airportTo, "AirBaltic", departureTime, arrivalTime);
+        Integer flightCountBefore = flightRepository.getFlights().size();
+        flightRepository.add(flight);
+
+        MockHttpServletRequestBuilder requestBuilder = post("/testing-api/clear")
+                .contentType(MediaType.APPLICATION_JSON);
+        mvc.perform(requestBuilder)
+                .andExpect(status().is(200))
+                .andReturn();
+        Integer flightCountAfter = flightRepository.getFlights().size();
+
+        assertEquals(flightCountBefore, flightCountAfter);
+    }
 }
