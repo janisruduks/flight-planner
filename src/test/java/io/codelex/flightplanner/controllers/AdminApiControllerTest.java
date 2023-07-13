48f1aadf129d670fe172e33e411ebceaa0823f1e
Jjanisruduks <janis.ruduks157@gmail.com>
2023-07-13 13:26:52 +0300

    Added integration for creating flights and clearing all flights
    Added unit test for airport search

--- src/test/java/io/codelex/flightplanner/controllers/AdminApiControllerTest.java
+++ src/test/java/io/codelex/flightplanner/controllers/AdminApiControllerTest.java
@@ -1,4 +1,129 @@
+package io.codelex.flightplanner.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.codelex.flightplanner.domain.Airport;
+import io.codelex.flightplanner.domain.Flight;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class AdminApiControllerTest {
-  
+
+    static ObjectMapper jsonObjectMapper = new ObjectMapper();
+    @Autowired
+    AdminApiController adminController;
+    @Autowired
+    TestingApiController testingController;
+    @Autowired
+    private MockMvc mvc;
+    @Value("${spring.security.user.name}")
+    private String username;
+    @Value("${spring.security.user.password}")
+    private String password;
+
+    @BeforeEach
+    public void clearFlights() {
+        testingController.clear();
+    }
+
+    @Test
+    @DisplayName("Should add and return flight with status code 201")
+    public void shouldAddFlight() throws Exception {
+        Flight mockFlight = createflight();
+        jsonObjectMapper.findAndRegisterModules();
+
+        MockHttpServletRequestBuilder requestBuilder = requestBuilder(mockFlight);
+        MvcResult result = mvcResult(requestBuilder, 201);
+        String responseContents = result.getResponse().getContentAsString();
+        Flight responseFlight = jsonObjectMapper.readValue(responseContents, Flight.class);
+
+        assertNotNull(responseFlight.getId());
+        assertEquals(responseFlight.getArrivalTime(), mockFlight.getArrivalTime());
+        assertEquals(responseFlight.getDepartureTime(), mockFlight.getDepartureTime());
+        assertEquals(responseFlight.getFrom(), mockFlight.getFrom());
+        assertEquals(responseFlight.getTo(), mockFlight.getTo());
+        assertEquals(responseFlight.getCarrier(), mockFlight.getCarrier());
+    }
+
+    @Test
+    @DisplayName("Should display 409 when adding the same person")
+    public void shouldNotFail() throws Exception {
+        Flight mockFlight = createflight();
+        jsonObjectMapper.findAndRegisterModules();
+
+        MockHttpServletRequestBuilder requestBuilder = requestBuilder(mockFlight);
+
+        mvcResult(requestBuilder, 201);
+        mvcResult(requestBuilder, 409);
+    }
+
+    @Test
+    @DisplayName("Should display 400 when adding flight with empty/null fields")
+    public void shouldNotAddFlightWithEmptyFields() throws Exception {
+        Flight mockFlight1 = createflight();
+        mockFlight1.setCarrier(null);
+        Flight mockFlight2 = null;
+        Flight mockFlight3 = createflight();
+        mockFlight3.setCarrier("");
+        jsonObjectMapper.findAndRegisterModules();
+
+        MockHttpServletRequestBuilder requestBuilder1 = requestBuilder(mockFlight1);
+        MockHttpServletRequestBuilder requestBuilder2 = requestBuilder(mockFlight2);
+        MockHttpServletRequestBuilder requestBuilder3 = requestBuilder(mockFlight3);
+
+        mvcResult(requestBuilder1, 400);
+        mvcResult(requestBuilder2, 400);
+        mvcResult(requestBuilder3, 400);
+    }
+
+    @Test
+    @DisplayName("Should fail on strange dates")
+    public void shouldFailOnStrangeDates() throws Exception {
+        Flight mockFlight = createflight();
+        mockFlight.setArrivalTime("1900-01-01 00:00");
+        jsonObjectMapper.findAndRegisterModules();
+
+        MockHttpServletRequestBuilder requestBuilder = requestBuilder(mockFlight);
+
+        mvcResult(requestBuilder, 400);
+    }
+
+    private Flight createflight() {
+        Airport airportFrom = new Airport("LV", "Riga", "Rix");
+        Airport airportTo = new Airport("IT", "Milan", "BGY");
+        return new Flight(
+                airportFrom,
+                airportTo,
+                "AirBaltic",
+                "2023-07-13 06:07",
+                "2023-07-16 12:04");
+    }
+
+    private MockHttpServletRequestBuilder requestBuilder(Flight mockFlight) throws Exception {
+        return put("/admin-api/flights", mockFlight)
+                .with(user(username).password(password))
+                .content(jsonObjectMapper.writeValueAsString(mockFlight))
+                .contentType(MediaType.APPLICATION_JSON);
+    }
+
+    private MvcResult mvcResult(MockHttpServletRequestBuilder requestBuilder, Integer statusCode) throws Exception {
+        return mvc.perform(requestBuilder)
+                .andExpect(status().is(statusCode))
+                .andReturn();
+    }
 }
--- src/test/java/io/codelex/flightplanner/services/FlightServiceTest.java
+++ src/test/java/io/codelex/flightplanner/services/FlightServiceTest.java
@@ -1,4 +1,62 @@
-import static org.junit.jupiter.api.Assertions.*;
+package io.codelex.flightplanner.services;
+
+import io.codelex.flightplanner.domain.Airport;
+import io.codelex.flightplanner.domain.Flight;
+import io.codelex.flightplanner.repository.FlightRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class FlightServiceTest {
-  
+
+    @Mock
+    FlightRepository flightRepository;
+
+    @InjectMocks
+    FlightService flightService;
+
+    private List<Flight> createMockFlights() {
+        Airport airportFrom1 = new Airport("Latvia", "Riga", "Rix");
+        Airport airportTo1 = new Airport("Italy", "Milan", "BGY");
+        Airport airportFrom2 = new Airport("England", "London", "LGW");
+        Airport airportTo2 = new Airport("Romania", "Bucharest", "OTP");
+        Flight flight1 = new Flight(airportFrom1, airportTo1, "AirBaltic", "2023-07-13 06:07", "2023-07-16 12:04");
+        Flight flight2 = new Flight(airportFrom2, airportTo2, "AirBaltic", "2023-07-13 06:07", "2023-07-16 12:04");
+
+        return List.of(flight1, flight2);
+    }
+
+    @Test
+    @DisplayName("Should search by incomplete phrases")
+    void searchForAirports() {
+        List<Flight> mockFlights = createMockFlights();
+        when(flightRepository.getFlights()).thenReturn(mockFlights);
+
+        List<Airport> result1 = flightService.getFilteredMatchList("latvia");
+        List<Airport> result2 = flightService.getFilteredMatchList("rig");
+        List<Airport> result3 = flightService.getFilteredMatchList(" ix");
+
+        assertEquals(1, result1.size());
+        assertEquals(mockFlights.get(0).getFrom().getAirport(), result1.get(0).getAirport());
+        assertEquals(mockFlights.get(0).getFrom().getCity(), result1.get(0).getCity());
+        assertEquals(mockFlights.get(0).getFrom().getCountry(), result1.get(0).getCountry());
+        assertEquals(1, result2.size());
+        assertEquals(mockFlights.get(0).getFrom().getAirport(), result2.get(0).getAirport());
+        assertEquals(mockFlights.get(0).getFrom().getCity(), result2.get(0).getCity());
+        assertEquals(mockFlights.get(0).getFrom().getCountry(), result2.get(0).getCountry());
+        assertEquals(1, result3.size());
+        assertEquals(mockFlights.get(0).getFrom().getAirport(), result3.get(0).getAirport());
+        assertEquals(mockFlights.get(0).getFrom().getCity(), result3.get(0).getCity());
+        assertEquals(mockFlights.get(0).getFrom().getCountry(), result3.get(0).getCountry());
+
+    }
 }
