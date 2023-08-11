23566ab66dff2f4a1893f3848ee4b933ea5b33d4
Jjanisruduks <janis.ruduks157@gmail.com>
2023-08-12 00:13:43 +0300

    Fixed insistencies

--- src/main/java/io/codelex/flightplanner/config/FlightPlannerConfiguration.java
+++ src/main/java/io/codelex/flightplanner/config/FlightPlannerConfiguration.java
@@ -4,24 +4,24 @@ import io.codelex.flightplanner.repository.DatabaseAirportRepository;
 import io.codelex.flightplanner.repository.DatabaseFlightRepository;
 import io.codelex.flightplanner.repository.InMemoryFlightRepository;
 import io.codelex.flightplanner.services.DatabaseFlightServiceImpl;
+import io.codelex.flightplanner.services.FlightService;
 import io.codelex.flightplanner.services.InMemoryFlightServiceImpl;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class FlightPlannerConfiguration {
 
     @Bean
     @ConditionalOnProperty(prefix = "flight-planner", name = "store-type", havingValue = "in-memory")
-    public InMemoryFlightServiceImpl getInMemoryService(InMemoryFlightRepository repository) {
+    public FlightService getInMemoryService(InMemoryFlightRepository repository) {
         return new InMemoryFlightServiceImpl(repository);
     }
 
     @Bean
     @ConditionalOnProperty(prefix = "flight-planner", name = "store-type", havingValue = "database")
-    public DatabaseFlightServiceImpl getDatabaseService(
+    public FlightService getDatabaseService(
             DatabaseFlightRepository flightRepository,
             DatabaseAirportRepository airportRepository
     ) {
--- src/main/java/io/codelex/flightplanner/repository/DatabaseAirportRepository.java
+++ src/main/java/io/codelex/flightplanner/repository/DatabaseAirportRepository.java
@@ -2,7 +2,22 @@ package io.codelex.flightplanner.repository;
 
 import io.codelex.flightplanner.entity.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DatabaseAirportRepository extends JpaRepository<Airport, Long> {
+
     Airport findByAirport(String airport);
+
+    @Query(value =
+            """
+                                select a
+                                from Airport a
+                                where lower(a.country) like %:match%
+                                or lower(a.airport) like %:match%
+                                or lower(a.city) like %:match%
+                    """
+    )
+    List<Airport> searchForMatchingAirports(String match);
 }
